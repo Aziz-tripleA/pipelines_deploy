109e45aa23ca7c4a5ac8a5f7fc29079fb8b0d469
ebrahimop <ebrahim.gomaa@o-projects.org>
2023-05-19 02:53:58 +0300

    create database

---
+++ Deploy_laravel_app_in_empty_ubuntu_server/deploy_nginx.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+apt update 
+apt upgrade
+# install latest php version
+apt install php
+# install php liberies 
+sudo apt install openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip php-pear php-fpm php-dev   php-xmlrpc php-gd  php-cli php-dom
+
+# remove apache if installed
+apt remove apache2 & apt purge apache2
+# install nginx 
+apt install nginx
+# install mysql
+apt install mysql-server
+
+
+if [ -f /root/.my.cnf ]; then
+	echo "Enter database name!"
+	read dbname
+    
+	echo "Creating new MySQL database..."
+	mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	echo "Database successfully created!"
+	
+	echo "Enter database user!"
+	read username
+    
+	echo "Enter the PASSWORD for database user!"
+	echo "Note: password will be hidden when typing"
+	read -s userpass
+    
+	echo "Creating new user..."
+	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	echo "User successfully created!"
+
+	echo "Granting ALL privileges on ${dbname} to ${username}!"
+	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -e "FLUSH PRIVILEGES;"
+	echo "You're good now :)"
+	exit
+	
+# If /root/.my.cnf doesn't exist then it'll ask for root password	
+else
+	echo "Please enter root user MySQL password!"
+	echo "Note: password will be hidden when typing"
+	read -s rootpasswd
+    
+	echo "Enter database name!"
+	read dbname
+    
+	echo "Creating new MySQL database..."
+	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	echo "Database successfully created!"
+    
+	echo "Enter database user!"
+	read username
+    
+	echo "Enter the PASSWORD for database user!"
+	echo "Note: password will be hidden when typing"
+	read -s userpass
+    
+	echo "Creating new user..."
+	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	echo "User successfully created!"
+	
+	echo "Granting ALL privileges on ${dbname} to ${username}!"
+	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+	echo "You're good now :)"
+    exist
